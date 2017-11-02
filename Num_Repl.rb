bab53e8e831eeaf4d8a704136a856940eec60244
Adrian Quintero <AdrianQ.life@gmail.com>
2017-11-02 19:04:00 -0400

    first commit

---
+++ Num_Repl.rb
@@ -0,0 +1,46 @@
+class Game
+  attr_reader :answer, :tries
+
+  def play
+    reset
+    puts "Welcome to the guessing game!!\n"
+    loop do
+      print "Guess a number between 1 and 100:"
+      guess = gets
+      guess = guess.chomp
+      break if guess == ""
+      check_guess(guess.to_i)
+    end
+    puts "Thank you for playing!"
+  end
+
+  def check_guess(guess)
+    if guess < answer
+      if answer - guess > 20
+        puts "you're WAY too low!"
+      else
+        puts "too low..."
+      end
+    elsif guess > answer
+      puts "too high..."
+    else
+      puts
+      puts "*******************"
+      puts "You got it! It took #{ tries } tries."
+      puts "*******************"
+      puts
+      reset
+    end
+    @tries = @tries + 1
+    # could also be written this way:
+    # @tries += 1
+  end
+
+  def reset
+    @tries  = 1
+    @answer = rand(1..100)
+  end
+end
+
+game = Game.new
+game.play
